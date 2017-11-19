0008cbf4af87d34bd5617d6b54d8b6d566a2b187
Benjamin-Bichel <bichel10@hotmail.com>
2017-11-18 23:00:47 -0500

    Fixed my own Merge errors for view

--- src/src/GUI/View.java
+++ src/src/GUI/View.java
@@ -6,9 +6,33 @@ import javax.swing.*;
 
 public class View {
     private JFrame mainWindow;
+
+    private JPanel dataHolder;
+    private JPanel dataManipulator;
+    private JPanel details;
+    private JPanel feature, mainPanel;
+
     public View(){
         mainWindow=new JFrame();
         mainWindow.setSize(420,420);
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        dataHolder = new JPanel();
+        dataManipulator = new JPanel();
+        details = new JPanel();
+        dataHolder.add(new JLabel("TempTest"));
+        dataHolder.setBorder(BorderFactory.createLineBorder(Color.black));
+        dataManipulator.add(new JLabel("TempTest2"));
+        dataManipulator.setBorder(BorderFactory.createLineBorder(Color.black));
+        details.add(new JLabel("TempTest3"));
+        details.setBorder(BorderFactory.createLineBorder(Color.black));
+
+
+        mainWindow.add(dataHolder);
+        mainWindow.add(dataManipulator);
+
+
         mainWindow.addWindowListener(new WindowAdapter() {
             //public void windowClosing(WindowEvent event);
         });

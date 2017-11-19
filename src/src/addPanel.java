caec1202a9bd4070dab6e448660ad56ec5387772
Benjamin-Bichel <bichel10@hotmail.com>
2017-11-19 02:07:17 -0500

    Working on add panel

--- src/src/addPanel.java
+++ src/src/addPanel.java
@@ -8,43 +8,45 @@ public class addPanel extends JPanel{
 
 
     public addPanel() {
-        JPanel addPanel = new JPanel();
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
         JLabel nameLabel = new JLabel("Name");
         nameLabel.setBounds(10,10,10,10);
-        addPanel.add(nameLabel);
+        this.add(nameLabel);
 
         nameText = new JTextField(20);
         nameText.setBounds(20,20,10,10);
-        addPanel.add(nameText);
+        this.add(nameText);
 
         JLabel volumeLabel = new JLabel("Volume");
         volumeLabel.setBounds(30,30,10,10);
-        addPanel.add(volumeLabel);
+        this.add(volumeLabel);
 
         volumeText = new JTextField(20);
         volumeText.setBounds(40,40,10,10);
-        addPanel.add(volumeText);
+        this.add(volumeText);
 
         JLabel priceLabel = new JLabel("Price");
         priceLabel.setBounds(50,50,10,10);
-        addPanel.add(priceLabel);
+        this.add(priceLabel);
 
         priceText = new JTextField(20);
         priceText.setBounds(60,60,10,10);
-        addPanel.add(priceText);
+        this.add(priceText);
 
         JLabel expiryLabel = new JLabel("Expiry");
         priceLabel.setBounds(70,70,10,10);
-        addPanel.add(expiryLabel);
+        this.add(expiryLabel);
 
         expiryText = new JTextField(20);
         expiryText.setBounds(80,80,10,10);
-        addPanel.add(expiryText);
+        this.add(expiryText);
 
         JButton addButton = new JButton("Add Item");
         addButton.setBounds(100, 100, 80, 25);
         addButton.addActionListener(new AddControler(this));
-        addPanel.add(addButton);
+        this.add(addButton);
     }
 
 }
